8f5b8b39a61b726241551de8881b42a611d43bc6
sealink <sealink@sealinkdeMac-mini.local>
2015-06-01 14:23:48 +0800

    plugin

--- daonala/platforms/android/assets/www/cordova_plugins.js
+++ daonala/platforms/android/assets/www/cordova_plugins.js
@@ -1,9 +1,17 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/nl.x-services.plugins.socialsharing/www/SocialSharing.js",
+        "id": "nl.x-services.plugins.socialsharing.SocialSharing",
+        "clobbers": [
+            "window.plugins.socialsharing"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.geolocation": "0.3.12"
+    "nl.x-services.plugins.socialsharing": "4.3.18"
 }
 // BOTTOM OF METADATA
 });
