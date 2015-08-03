89e5985ed7426769e59c5c82fd05c70f849d451c
shosky <wx5753@gmail.com>
2015-08-03 19:53:21 +0800

    no message

--- daonala/platforms/ios/www/assets/js/public/iscroll.js
+++ daonala/platforms/ios/www/assets/js/public/iscroll.js
@@ -81,7 +81,7 @@ function custLoaded(){
                 }
             }
         });
-        custboardScroll.scrollToTop(100);
+       // custboardScroll.scrollToTop(100);
     }
 }
 
@@ -226,3 +226,25 @@ function historyLoaded() {
 }
 
 //document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+
+var traceScroll;
+function traceScroll(){
+    if(traceScroll==undefined || traceScroll == null) {
+        var traceScroll = new IScroll('#tracewrapper', {
+            probeType: 2,//probeType：1对性能没有影响。在滚动事件被触发时，滚动轴是不是忙着做它的东西。
+            // probeType：2总执行滚动，除了势头，反弹过程中的事件。这类似于原生的onscroll事件。
+            // probeType：3发出的滚动事件与到的像素精度。
+            // 注意，滚动被迫requestAnimationFrame（即：useTransition：假）。
+            scrollbars: true,//有滚动条
+            mouseWheel: true,//允许滑轮滚动
+            fadeScrollbars: true,//滚动时显示滚动条，默认影藏，并且是淡出淡入效果
+            bounce:true,//边界反弹
+            interactiveScrollbars:true,//滚动条可以拖动
+            shrinkScrollbars:'scale',// 当滚动边界之外的滚动条是由少量的收缩。'clip' or 'scale'.
+            click: true ,// 允许点击事件
+            keyBindings:true,//允许使用按键控制
+            momentum:true// 允许有惯性滑动
+        });
+    }
+}
+
